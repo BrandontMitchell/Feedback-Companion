75cb5e4dadd5021b6f35515945061442bd73d8ae
Brandon Mitchell <btmbaseball99@gmail.com>
2019-12-07 18:29:49 -0800

    refactor and separate routes, services, and config

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
-
+require('./services/passport');
+require('./routes/authRoutes');
 
 const app = express();
 
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
