f0ffcc01794a1bdb9b5dc4de16c8985e138079bf
Brandon Mitchell <btmbaseball99@gmail.com>
2019-12-07 19:25:04 -0800

    add mongoose implementation

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
-require('./routes/authRoutes');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-require('./routes/authRoutes')(app);
+
+require('./routes/authRoutes')(app); // instead of assigning a const, we can directly apply an 
+                                    // argument to the require statement
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
