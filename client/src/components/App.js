4bad9daf886754719aa9b1d524ff885d72480253
Brandon Mitchell <btmbaseball99@gmail.com>
2019-12-09 18:55:19 -0800

    exact path refactor for landing page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,7 +12,7 @@ const App = () => {
         <div>
             <BrowserRouter> 
             <div>
-                <Route path="/" component={Landing} />
+                <Route exact path="/" component={Landing} />
                 <Route path="/surveys" component={Dashboard} />
             </div>
             </BrowserRouter>
