980938aba3be7948dda1dcd97a96a4c939e90776
Brandon Mitchell <btmbaseball99@gmail.com>
2019-12-10 12:15:43 -0800

    add cases for logged in / out in header

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,8 +1,12 @@
+import { FETCH_USER } from '../actions/types';
+
 // records whether or not the user is logged in
 
 export default function(state = {}, action) {
     console.log(action);
     switch (action.type) {
+        case FETCH_USER: 
+            return action.payload || false;
         default: 
             return state;
     }
