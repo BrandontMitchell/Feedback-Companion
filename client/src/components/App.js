56a096af89e3e0b08dedf51fe95297ace35f28a4
Brandon Mitchell <btmbaseball99@gmail.com>
2019-12-09 23:16:56 -0800

    test payload with loggedin user via dispatch

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Header from './Header';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+import Header from "./Header";
 // BrowserRouter tells router how to change, Route used to setup rule between action and what will happen
 // Browser only can have max one child component!
 
-const Dashboard = () => <h2>Dashboard</h2>
-const SurveyNew = () => <h2>SurveyNew</h2>
-const Landing = () => <h2>Landing</h2>
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
+  render() {
     return (
-        <div className="container">
-            <BrowserRouter> 
-            <div>
-                <Header />
-                <Route exact path="/" component={Landing} />
-                <Route exact path="/surveys" component={Dashboard} />
-                <Route path="/surveys/new" component={SurveyNew} />
-            </div>
-            </BrowserRouter>
-        </div>
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
     );
-};
+  }
+}
 
-export default App;
+export default connect(null, actions)(App);
