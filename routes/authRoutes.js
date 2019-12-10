cdc9b3740a233c448976b9d1dbaf152831fd1846
Brandon Mitchell <btmbaseball99@gmail.com>
2019-12-10 12:28:50 -0800

    fix redirect after successful login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,13 @@ module.exports = app => {
     })
   );
 
-  app.get("/auth/google/callback", passport.authenticate("google"));
+  app.get(
+    "/auth/google/callback", 
+    passport.authenticate("google"),
+    (req, res) => { //redirect after login complete
+      res.redirect('/surveys'); // res = response object, redirect responds to browsers request
+    }
+  );
 
   app.get("/api/logout", (req, res) => {
     req.logout();
