d69c74773814a16c7efa3b76047ce089d15c2d41
Brandon Mitchell <btmbaseball99@gmail.com>
2019-12-09 18:29:07 -0800

    add reducers for redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import App from "./components/App";
+import reducers from './reducers';
+// redux store
+// Reducers specify how the application's state changes in response to actions sent to the store
+const store = createStore(reducers, {}, applyMiddleware());
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+
+ReactDOM.render(
+    <Provider store={store}><App /></Provider>,  // react component that reads redux changes
+    document.querySelector("#root")
+);
