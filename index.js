3fc020292c180c5e1dd1ee5ea8d758ffb844a82f
Brandon Mitchell <btmbaseball99@gmail.com>
2019-12-12 16:48:24 -0800

    add production server side architecture

--- index.js
+++ index.js
@@ -27,5 +27,18 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // express will serve production assets
+  // like our main.js or main.css file
+  app.use(express.static('client/build'));
+  //express will serve up index.html if it doesn't recognize route
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
