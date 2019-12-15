50dd0aacd4d58855121e3cc0609e68ff7c7550cd
Brandon Mitchell <btmbaseball99@gmail.com>
2019-12-15 09:29:56 -0800

    survey hierarchy inside of comps, separate fields

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,10 @@
+// individual survey fields --> renders single label and textfield
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            <input />
+        </div>
+    );
+};
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,14 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import SurveyField from './SurveyField';
 
 class SurveyForm extends Component {
-    render() {
-        return (
-            <div>
-                Survey Form -_-
-            </div>
-        );
-    }
+
+  renderFields() {
+      return (
+          <div>
+            <Field type="text" name="title" component={SurveyField}/>
+          </div>
+      );
+  }
+
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
-
-export default SurveyForm;
+export default reduxForm({
+  form: "surveyForm"
+})(SurveyForm);
