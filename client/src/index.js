7c7c161104a55596f0fb7600bddabbd209574c7d
Brandon Mitchell <btmbaseball99@gmail.com>
2019-12-18 09:29:55 -0800

    minor refactor

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,21 @@
-import 'materialize-css/dist/css/materialize.min.css';
+import "materialize-css/dist/css/materialize.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
 import App from "./components/App";
-import reducers from './reducers';
-import axios from 'axios';
+import reducers from "./reducers";
+import axios from "axios";
 window.axios = axios;
-
-
 
 // redux store
 // Reducers specify how the application's state changes in response to actions sent to the store
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
-
 ReactDOM.render(
-    <Provider store={store}><App /></Provider>,  // react component that reads redux changes
-    document.querySelector("#root")
+  <Provider store={store}>
+    <App />
+  </Provider>, // react component that reads redux changes
+  document.querySelector("#root")
 );
